d705c83c2c57b0c4a242f8fa863d973c45d2fa50
atul24112001 <atulmorchhlay204@gmail.com>
2025-03-26 15:28:53 +0530

    Refactor context provider and update import paths for layout and page components

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/view/layout/navbar";
-import { Provider } from "./provider";
+import { Provider } from "../context/provider";
 import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
